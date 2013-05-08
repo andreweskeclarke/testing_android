bab59c234bfc9ef2db3e53e032460bf28a4ce3e3
andreweskeclarke <andreweskeclarke@gmail.com>
2013-05-08 10:02:20 -0300

    Added Robotium project

---
+++ TestingAndroidRobotium/src/com/aeclarke/testingandroid/test/TestMainActivity.java
@@ -0,0 +1,30 @@
+package com.aeclarke.testingandroid.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.aeclarke.testingandroid.MainActivity;
+import com.jayway.android.robotium.solo.Solo;
+
+import junit.framework.TestCase;
+
+public class TestMainActivity extends ActivityInstrumentationTestCase2<MainActivity>{ 
+	private Solo solo;
+	
+	public TestMainActivity(){ 
+		super(MainActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+	
+	public void testNavigatingToLogin() {
+		solo.clickOnButton("Login");
+	}
+
+	protected void tearDown() throws Exception {
+		solo.finishOpenedActivities();
+	}
+
+}
