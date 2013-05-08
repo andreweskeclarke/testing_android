a9d5bedeb80ddec24daa1aa3c7402271edd39353
andreweskeclarke <andreweskeclarke@gmail.com>
2013-05-07 23:16:02 -0300

    Adding test to navigate to login page.  Added gitignore rules

--- TestingAndroid/src/com/aeclarke/testingandroid/MainActivity.java
+++ TestingAndroid/src/com/aeclarke/testingandroid/MainActivity.java
@@ -1,13 +1,29 @@
 package com.aeclarke.testingandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	private Button loginButton;
+	private OnClickListener loginListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+			startActivity(intent);
+		}
+	};
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		loginButton = (Button) findViewById(R.id.login_button);
+		loginButton.setOnClickListener(loginListener );
 	}
 }
--- TestingAndroid/test/com/aeclarke/testingandroid/MyActivityTest.java
+++ TestingAndroid/test/com/aeclarke/testingandroid/MyActivityTest.java
@@ -1,20 +1,49 @@
 package com.aeclarke.testingandroid;
 
+import android.content.Intent;
+import android.widget.Button;
+
 import com.aeclarke.testingandroid.MainActivity;
 import com.aeclarke.testingandroid.R;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowIntent;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.robolectric.Robolectric.shadowOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 @RunWith(RobolectricTestRunner.class)
 public class MyActivityTest {
 
-    @Test
+	private MainActivity activity;
+	private Button loginButton;
+	
+    @Before
+    public void setUp() throws Exception {
+    	activity = new MainActivity();
+    	activity.onCreate(null);
+    	loginButton = (Button) activity.findViewById(R.id.login_button);
+    }
+	
+	@Test
     public void shouldHaveWelcomeText() throws Exception {
-        String hello = new MainActivity().getResources().getString(R.string.main_text);
+        String hello = activity.getResources().getString(R.string.main_text);
         assertThat(hello, equalTo("Welcome to Testing Android"));
     }
+   
+	@Test
+	public void pressingTheLoginButtonShouldNavigateToLoginActivity() throws Exception {
+		loginButton.performClick();
+		
+		ShadowActivity shadowActivity = shadowOf(activity);
+		Intent startedIntent = shadowActivity.getNextStartedActivity();
+		ShadowIntent shadowIntent = shadowOf(startedIntent);
+		
+		assertThat(shadowIntent.getComponent().getClassName(), equalTo(LoginActivity.class.getName()));
+	}
+    
 }
