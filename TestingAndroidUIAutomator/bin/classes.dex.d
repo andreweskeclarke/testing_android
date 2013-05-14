1cf5ee8faaad7ed5fd7b85f55f94fd37072d6625
andreweskeclarke <andreweskeclarke@gmail.com>
2013-05-14 16:28:02 -0500

    Git ignoring bin directory

--- TestingAndroidUIAutomator/bin/classes.dex.d
+++
@@ -1,2 +0,0 @@
-/Users/Thoughtworker/Programming/android/workspace/testing_android/TestingAndroidUIAutomator/bin/classes.dex : \
-/Users/Thoughtworker/Programming/android/workspace/testing_android/TestingAndroidUIAutomator/bin/classes/com/aeclarke/testingandroid/MainActivityTest.class \
